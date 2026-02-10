2678d626aaaf3106db87bff0e2c4485f10e2663f
jamiejcross <jamiejcross@gmail.com>
2026-02-10 00:59:49 +0000

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -4,45 +4,71 @@ import datetime
 import re
 import time
 
-# CATEGORIES AND FEEDS
+# CATEGORIES AND FEEDS (Updated with Biotechnology, Food Science, and Vascular Review)
 RSS_FEEDS = {
     "science": [
-        "https://www.nature.com/srep.rss", "https://www.nature.com/ncomms.rss",
-        "https://journals.plos.org/plosone/feed/atom", "https://www.mdpi.com/rss/journal/jof",
-        "https://www.mdpi.com/rss/journal/microorganisms", "https://www.mdpi.com/rss/journal/molecules",
-        "https://www.mdpi.com/rss/journal/biomolecules", "https://journals.asm.org/action/showFeed?feed=rss&jc=MBIO&type=etoc",
-        "https://journals.asm.org/action/showFeed?feed=rss&jc=SPECTRUM&type=etoc", "https://www.frontiersin.org/journals/microbiology/rss",
-        "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=acsodf", "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=jacsat",
-        "https://rss.sciencedirect.com/publication/science/01418130", "https://www.mdpi.com/rss/journal/ijms"
+        "https://www.nature.com/srep.rss", 
+        "https://www.nature.com/ncomms.rss",
+        "https://journals.plos.org/plosone/feed/atom", 
+        "https://www.mdpi.com/rss/journal/jof",
+        "https://www.mdpi.com/rss/journal/microorganisms", 
+        "https://www.mdpi.com/rss/journal/molecules",
+        "https://onlinelibrary.wiley.com/action/showFeed?jc=10970010&type=etoc&feed=rss", # J. Sci Food Agric
+        "https://www.mdpi.com/rss/journal/biomolecules", 
+        "https://www.mdpi.com/rss/journal/ijms",
+        "https://journals.asm.org/action/showFeed?feed=rss&jc=MBIO&type=etoc",
+        "https://journals.asm.org/action/showFeed?feed=rss&jc=SPECTRUM&type=etoc", 
+        "https://www.frontiersin.org/journals/microbiology/rss",
+        "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=acsodf", 
+        "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=jacsat",
+        "https://rss.sciencedirect.com/publication/science/01418130"
     ],
     "media": [
         "https://www.sciencedaily.com/rss/plants_animals/fungi.xml",
         "https://phys.org/rss-feed/biology-news/microbiology/"
     ],
     "health": [
-        "https://www.mdpi.com/rss/journal/toxins", "https://www.cdc.gov/media/rss/topic/fungal.xml",
-        "https://www.mdpi.com/rss/journal/animals", "https://www.mdpi.com/rss/journal/plants",
-        "https://www.mdpi.com/rss/journal/agronomy", "https://www.frontiersin.org/journals/plant-science/rss",
-        "https://www.frontiersin.org/journals/veterinary-science/rss", "https://www.mdpi.com/rss/journal/foods",
-        "https://rss.sciencedirect.com/publication/science/03088146", "https://rss.sciencedirect.com/publication/science/09639969",
-        "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=jafcau", "https://www.mdpi.com/rss/journal/marinedrugs"
+        "https://www.mdpi.com/rss/journal/toxins", 
+        "https://www.cdc.gov/media/rss/topic/fungal.xml",
+        "https://www.mdpi.com/rss/journal/animals", 
+        "https://www.mdpi.com/rss/journal/plants",
+        "https://www.mdpi.com/rss/journal/agronomy", 
+        "https://www.frontiersin.org/journals/plant-science/rss",
+        "https://www.frontiersin.org/journals/veterinary-science/rss", 
+        "https://www.mdpi.com/rss/journal/foods",
+        "https://rss.sciencedirect.com/publication/science/03088146", 
+        "https://rss.sciencedirect.com/publication/science/09639969",
+        "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=jafcau", 
+        "https://www.mdpi.com/rss/journal/marinedrugs"
     ],
     "indoor": [
-        "https://journals.asm.org/action/showFeed?feed=rss&jc=AEM&type=etoc", "https://www.ashrae.org/RssFeeds/news-feed.xml",
+        "https://journals.asm.org/action/showFeed?feed=rss&jc=AEM&type=etoc", 
+        "https://www.ashrae.org/RssFeeds/news-feed.xml",
         "https://www.gov.uk/search/news-and-communications.atom?content_store_document_type=news_story&organisations[]=department-for-levelling-up-housing-and-communities",
-        "https://www.mdpi.com/rss/journal/fermentation", "https://www.mdpi.com/rss/journal/catalysts",
-        "https://www.mdpi.com/rss/journal/applsci", "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=jnprdf",
-        "https://rss.sciencedirect.com/publication/science/09608524", "https://rss.sciencedirect.com/publication/science/00489697",
-        "https://rss.sciencedirect.com/publication/science/02697491", "https://rss.sciencedirect.com/publication/science/09608524",
-        "https://rss.sciencedirect.com/publication/science/03043894", "https://rss.sciencedirect.com/publication/science/13858947",
+        "https://www.mdpi.com/rss/journal/fermentation", 
+        "https://www.mdpi.com/rss/journal/catalysts",
+        "https://www.mdpi.com/rss/journal/applsci", 
+        "https://pubs.acs.org/action/showFeed?type=etoc&feed=rss&jc=jnprdf",
+        "https://rss.sciencedirect.com/publication/science/09608524", 
+        "https://rss.sciencedirect.com/publication/science/00489697",
+        "https://rss.sciencedirect.com/publication/science/02697491", 
+        "https://rss.sciencedirect.com/publication/science/03043894", 
+        "https://rss.sciencedirect.com/publication/science/13858947",
         "https://rss.sciencedirect.com/publication/science/09619534"
     ],
     "clinical": [
-        "https://journals.plos.org/plospathogens/feed/atom", "https://www.mdpi.com/rss/journal/antibiotics",
-        "https://www.mdpi.com/rss/journal/pathogens", "https://www.mdpi.com/rss/journal/pharmaceuticals",
-        "https://www.mdpi.com/rss/journal/diagnostics", "https://journals.asm.org/action/showFeed?feed=rss&jc=AAC&type=etoc",
-        "https://journals.asm.org/action/showFeed?feed=rss&jc=JCM&type=etoc", "https://www.frontiersin.org/journals/cellular-and-infection-microbiology/rss",
-        "https://www.frontiersin.org/journals/medicine/rss", "https://www.frontiersin.org/journals/immunology/rss",
+        "https://journals.plos.org/plospathogens/feed/atom", 
+        "https://www.benthamdirect.com/content/journals/cpb/fasttrack?feed=rss", # Current Pharm. Biotech
+        "https://verjournal.com/index.php/ver/gateway/plugin/WebFeedGatewayPlugin/rss2", # Vascular & Endo Review
+        "https://www.mdpi.com/rss/journal/antibiotics",
+        "https://www.mdpi.com/rss/journal/pathogens", 
+        "https://www.mdpi.com/rss/journal/pharmaceuticals",
+        "https://www.mdpi.com/rss/journal/diagnostics", 
+        "https://journals.asm.org/action/showFeed?feed=rss&jc=AAC&type=etoc",
+        "https://journals.asm.org/action/showFeed?feed=rss&jc=JCM&type=etoc", 
+        "https://www.frontiersin.org/journals/cellular-and-infection-microbiology/rss",
+        "https://www.frontiersin.org/journals/medicine/rss", 
+        "https://www.frontiersin.org/journals/immunology/rss",
         "https://www.frontiersin.org/journals/pharmacology/rss"
     ]
 }
